fcd68469bf09ef7350f3b451663a65891b5866ca
Phodal Huang <h@phodal.com>
2020-01-01 11:20:19 +0800

    poc: add a rust web for test

--- web/index.js
+++
@@ -1,4 +0,0 @@
-const js = import("./node_modules/domain/domain.js");
-js.then(js => {
-    js.greet("WebAssembly");
-});
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,9 +1,29 @@
-const path = require('path');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+const dist = path.resolve(__dirname, "dist");
+
 module.exports = {
-    entry: "./index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "index.js",
-    },
-    mode: "development"
+  mode: "production",
+  entry: {
+    index: "./js/index.js"
+  },
+  output: {
+    path: dist,
+    filename: "[name].js"
+  },
+  devServer: {
+    contentBase: dist,
+  },
+  plugins: [
+    new CopyPlugin([
+      path.resolve(__dirname, "static")
+    ]),
+
+    new WasmPackPlugin({
+      crateDirectory: __dirname,
+      extraArgs: "--out-name index"
+    }),
+  ]
 };
